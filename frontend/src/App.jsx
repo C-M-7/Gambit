0b3e64cab3e5fff3a88c323219be5365a61cc3a6
C-M-7 <chiranm007@gmail.com>
2024-06-26 16:45:21 +0530

    sorted the auth feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,89 +3,105 @@ import Login from "./pages/Login.jsx";
 import Home from "./pages/Home.jsx";
 import Cookies from "js-cookie";
 import axios from "axios";
-import {toast} from 'sonner';
+import { toast } from "sonner";
 import { io } from "socket.io-client";
 import { useContext, useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setUserDetails } from "./redux/slices/UserDetails.jsx";
 import Playground from "./pages/Playground.jsx";
 import SocketContext from "./redux/SocketContext.jsx";
 import Signup from "./pages/Signup.jsx";
 
 function App() {
-  const {setSocketContext} = useContext(SocketContext);  
+  const { setSocketContext } = useContext(SocketContext);
   const [isUser, setIsuser] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
-  console.log('hi app');
+  const userdetails = useSelector((state) => state.UserDetails);
 
   const getUserInfo = async (token) => {
     try {
       const response = await axios.post("/gambit/userinfo", {
-        token : token
+        token: token,
       });
       if (response.data.status) {
         dispatch(setUserDetails(response.data.user));
         setIsuser(true);
       }
     } catch (err) {
-      console.log(err.message)
-    }finally {
-      setIsLoading(false); 
+      console.log(err.message);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
     const token = Cookies.get("token");
-    if (token) {
-      getUserInfo(token);
-      try{
-        const socketInstance = io('http://localhost:7000',{
-          auth:{
-            token : token
-          }
-        })
-        console.log(socketInstance);
-        setSocketContext(socketInstance);
-
-        socketInstance.on('connect', () => {
-          console.log('Connected to socketInstance server');
-        });
-
-        socketInstance.on('disconnect',()=>{
-          // Cookies.remove("token");
-          socketInstance.disconnect();
-          console.log("user disconnected from server")
-        });
-        
-        return () =>{
-          socketInstance.off('connect');
-          socketInstance.off('disconnect');
-        }
-      }
-      catch(err){
-        toast.warning('Unable to connect at the moment please try again later');
-        <Navigate to='/signin'/>
-      }
-    }else{
+    if (userdetails.username !== null && Object.keys(userdetails).length > 0) {
+      setIsuser(true);
       setIsLoading(false);
-      toast.error('Token not found! Please SigIn again!');
+    } else {
+      if (token) {
+        getUserInfo(token);
+        try {
+          const socketInstance = io("http://localhost:7000", {
+            auth: {
+              token: token,
+            },
+          });
+          console.log(socketInstance);
+          setSocketContext(socketInstance);
+
+          socketInstance.on("connect", () => {
+            console.log("Connected to socketInstance server");
+          });
+
+          socketInstance.on("disconnect", () => {
+            // Cookies.remove("token");
+            socketInstance.disconnect();
+            console.log("user disconnected from server");
+          });
+
+          return () => {
+            socketInstance.off("connect");
+            socketInstance.off("disconnect");
+          };
+        } catch (err) {
+          toast.warning(
+            "Unable to connect at the moment please try again later"
+          );
+          <Navigate to="/signin" />;
+        }
+      } else {
+        setIsLoading(false);
+        toast.error("Token not found! Please SigIn again!");
+      }
     }
   }, []);
 
   if (isLoading) {
-    return <div>Loading...</div>;  
+    return <div>Loading...</div>;
   }
-  
+
   return (
     <>
       <Routes>
-        <Route path="/signin" element={<Login/>}/>
-        <Route path="/home" element={!isUser ? <Navigate to='/signin'/> : 
-          <Home/>}/>
-        <Route path="/" element={!isUser ? <Navigate to='/signin'/> : <Navigate to='/home'/>}/>
-        <Route path="/playground" element={!isUser ? <Navigate to='/signin'/> : <Playground/>}/>
-        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/signin" element={<Login />} />
+        <Route
+          path="/home"
+          element={userdetails.username==null ? <Navigate to="/signin" /> : <Home />}
+        />
+        <Route
+          path="/"
+          element={
+            !isUser ? <Navigate to="/signin" /> : <Navigate to="/home" />
+          }
+        />
+        <Route
+          path="/playground"
+          element={!isUser ? <Navigate to="/signin" /> : <Playground />}
+        />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </>
   );
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,50 +1,52 @@
 import React, { useEffect, useState } from "react";
-import {Link, useNavigate} from 'react-router-dom';
-import axios from 'axios';
-import {toast} from 'sonner';
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "sonner";
+import { useDispatch } from "react-redux";
+import { setUserDetails } from "../redux/slices/UserDetails";
 
 function Login() {
   const navigate = useNavigate();
-  const [mail, setMail] = useState('');
-  const [password, setPassword] = useState('');
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
 
-  // useEffect(()=>{
+  const handleMail = (event) => {
+    setMail(event.target.value);
+  };
 
-  // })
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
 
-  const handleMail = (event) =>{
-      setMail(event.target.value);
-  }
-
-  const handlePassword = (event) =>{
-      setPassword(event.target.value);
-  }
-
-  const handleClick = async(event)=>{
+  const handleClick = async (event) => {
     event.preventDefault();
-    if(mail === '' || password === ''){
-        setMail('');
-        setPassword('');
-        toast.error('All fields are mandatory!');
+    if (mail === "" || password === "") {
+      setMail("");
+      setPassword("");
+      toast.error("All fields are mandatory!");
+    } else {
+      try {
+        const response = await axios.post("/gambit/signin/", {
+          email: mail,
+          password: password,
+        });
+        console.log(response.data);
+        if (response) {
+          dispatch(setUserDetails(response.data));
+          navigate("/home");
+          toast.success("SignIn was successful!");
+        }
+      } catch (err) {
+        console.error("Error :", err.response.data);
+      }
     }
-    else{
-        try{
-            const response = await axios.post('/gambit/signin/',{
-                email : mail,
-                password : password
-            })
-            console.log(response);
-            if(response){
-              navigate('/home');
-              toast.success('SignIn was successful!');
-            }
+  };
 
-        }
-        catch(err){
-            console.error('Error :', err.response.data);
-        }
-    }
-}
+  // if (loading) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen">
@@ -52,20 +54,36 @@ function Login() {
         <div className="text-3xl font-bold">SignIn</div>
         <div>
           <div>Email</div>
-          <input className='border border-black w-72 h-8 px-1' type='text' value={mail} onChange={handleMail}/>
+          <input
+            className="border border-black w-72 h-8 px-1"
+            type="text"
+            value={mail}
+            onChange={handleMail}
+          />
         </div>
         <div>
           <div>Password</div>
-          <input className='border border-black w-72 h-8 px-1' type='text' value={password}  onChange={handlePassword}/>
+          <input
+            className="border border-black w-72 h-8 px-1"
+            type="text"
+            value={password}
+            onChange={handlePassword}
+          />
         </div>
         <div>
-        <button className='border border-black rounded-md h-10 mt-8 w-40 hover:bg-black hover:text-white transition transform active:scale-95 focus:outline-none' onClick={handleClick}>
+          <button
+            className="border border-black rounded-md h-10 mt-8 w-40 hover:bg-black hover:text-white transition transform active:scale-95 focus:outline-none"
+            onClick={handleClick}
+          >
             SignIn
           </button>
         </div>
       </div>
       <div>
-        Don't have an account? <Link to={'/signup'} className="underline font-bold">Signup here</Link>
+        Don't have an account?{" "}
+        <Link to={"/signup"} className="underline font-bold">
+          Signup here
+        </Link>
       </div>
     </div>
   );
