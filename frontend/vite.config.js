b64426610c18e446552183af9f795b24b262547f
C-M-7 <chiranm007@gmail.com>
2024-07-17 19:42:26 +0530

    vite config-5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host:true,
-    port:5173,
+    host: true,
+    port: 5173,
     proxy: {
-
-      '/gambit': {
-        target : 'https://gambit.strangled.net',
-        changeOrigin : true,
-        secure : true,
+      "/gambit": {
+        target: "https://gambit.strangled.net",
+        changeOrigin: true,
+        secure: true,
       },
-    }
+    },
   },
-})
+});
