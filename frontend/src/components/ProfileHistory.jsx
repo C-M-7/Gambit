943cd680665a89e9a252b67373539e4540557aae
C-M-7 <chiranm007@gmail.com>
2024-07-19 22:27:12 +0530

    profile features

--- frontend/src/components/ProfileHistory.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ProfileHistory = () =>{
-    return <div>history</div>
-}
-
-export default ProfileHistory
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import { useSelector } from "react-redux";
 import ProfileMenu from "../components/ProfileMenu";
+import chess_bg from '../utils/chess-bg.jpg'; 
 
 function Home() {
   const { socketContext } = useContext(SocketContext);
